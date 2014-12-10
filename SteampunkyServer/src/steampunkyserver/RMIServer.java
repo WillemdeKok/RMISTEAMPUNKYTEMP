3bef9611a55c8a7b0d8c932b95c0098dbdba37f2
Bart123456789 <Voetbal_bart10@hotmail.com>
2014-12-10 12:57:55 +0100

    Rmi pull server added

---
+++ SteampunkyServer/src/steampunkyserver/RMIServer.java
@@ -0,0 +1,154 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.rmi.Naming;
+import java.util.Enumeration;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ * Example of RMI using Registry
+ *
+ * @author Bart
+ */
+public class RMIServer {
+
+    private static boolean createRegistry = true;
+    // Set port number
+    private static final int portNumber = 1099;
+
+    // Set binding name for student administration
+    private static final String bindingName = "MockEffectenbeurs";
+
+    // References to registry and student administration
+    private Registry registry = null;
+    private MockEffectenbeurs mock;
+
+    // Constructor
+    public RMIServer() {
+        System.out.println("Server: Port number " + portNumber);
+        try {
+            mock = new MockEffectenbeurs();
+            System.out.println("Server: MockEffectenbeurs created");
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot create MockEffectenbeurs");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+            mock = null;
+        }
+        //Aanmaken van een regisrty
+        if (createRegistry) {
+            registry = createRegistry();
+      
+        // Als register aan maken is gelukt proberen te binden op register
+        if (registry != null && mock != null) {
+                bindMockUsingRegistry();
+                System.out.println("Server: MockEffectenbeurs bound to " + bindingName);
+            } else {
+                System.out.println("Server: MockEffectenbeurs not bound");
+            }
+        } 
+        // Als register aan maken is gelukt proberen te binden op naam
+        else {
+            if (mock != null) {
+                bindMockUsingNaming();
+                System.out.println("Server: MockEffectenbeurs bound to " + bindingName);
+            } else {
+                System.out.println("Server: MockEffectenbeurs not bound");
+            }
+        }
+    }
+
+    //Maakt een register aan de hand van het poort nummer
+    private Registry createRegistry() {
+        Registry registry = null;
+        try {
+            registry = LocateRegistry.createRegistry(portNumber);
+            System.out.println("Server: Registry created on port number " + portNumber);
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot create registry");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+            registry = null;
+        }
+        return registry;
+    }
+
+    // probeert het register tebinden op bindingnaam en de classe met de RemotePublisher
+    private void bindMockUsingRegistry() {
+        try {
+            registry.rebind(bindingName, mock);
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot bind MockEffectenbeurs");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+        }
+    }
+    
+    // probeert het register tebinden op naam en de classe met de RemotePublisher
+    private void bindMockUsingNaming() {
+        try {
+            LocateRegistry.createRegistry(portNumber);
+            Naming.rebind(bindingName, mock);
+        } catch (MalformedURLException ex) {
+            System.out.println("Server: Cannot bind MockEffectenbeurs");
+            System.out.println("Server: MalformedURLException: " + ex.getMessage());
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot bind MockEffectenbeurs");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+        }
+    }
+
+    private static void printIPAddresses() {
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            System.out.println("Server: IP Address: " + localhost.getHostAddress());
+            InetAddress[] allMyIps = InetAddress.getAllByName(localhost.getCanonicalHostName());
+            if (allMyIps != null && allMyIps.length > 1) {
+                System.out.println("Server: Full list of IP addresses:");
+                for (InetAddress allMyIp : allMyIps) {
+                    System.out.println("    " + allMyIp);
+                }
+            }
+        } catch (UnknownHostException ex) {
+            System.out.println("Server: Cannot get IP address of local host");
+            System.out.println("Server: UnknownHostException: " + ex.getMessage());
+        }
+
+        try {
+            System.out.println("Server: Full list of network interfaces:");
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                System.out.println("    " + intf.getName() + " " + intf.getDisplayName());
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    System.out.println("        " + enumIpAddr.nextElement().toString());
+                }
+            }
+        } catch (SocketException ex) {
+            System.out.println("Server: Cannot retrieve network interface list");
+            System.out.println("Server: UnknownHostException: " + ex.getMessage());
+        }
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("SERVER USING REGISTRY");
+
+        // Print IP addresses and network interfaces
+        printIPAddresses();
+
+        // Create server
+        RMIServer server = new RMIServer();
+    }
+}
