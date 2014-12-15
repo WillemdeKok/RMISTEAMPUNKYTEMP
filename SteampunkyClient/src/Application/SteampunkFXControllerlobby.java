7e6629366607cd7806b8be17f3b9df449a71d335
cyrilbrugman <cyril.brugman@gmail.com>
2014-12-15 14:41:35 +0100

    lelijker

--- SteampunkyClient/src/Application/SteampunkFXControllerlobby.java
+++ SteampunkyClient/src/Application/SteampunkFXControllerlobby.java
@@ -5,6 +5,7 @@
  */
 package Application;
 
+
 import java.net.URL;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -21,37 +22,27 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javax.swing.JOptionPane;
 
+
 /**
  *
  * @author Bart
  */
-public class SteampunkFXControllerlobby implements Initializable {
-
+public class SteampunkFXControllerlobby implements Initializable
+{
     //Lobby
-
-    @FXML
-    Label Creatlobbynamelb;
-    @FXML
-    Label Creatlobbypasswordlb;
-    @FXML
-    Label joinlobbynamelb;
-    @FXML
-    Label LBLLobbyWelcome;
-    @FXML
-    ComboBox CBjoinlobby;
-    @FXML
-    TextField TFchat;
-    @FXML
-    TextField TfCreatename;
-    @FXML
-    TextField Tfvreatepassword;
-    @FXML
-    Button Btcreatelobby;
-    @FXML
-    Button btjoinlobby;
-    @FXML
-    ListView Lblobby;
-
+    @FXML Label Creatlobbynamelb;
+    @FXML Label Creatlobbypasswordlb;
+    @FXML Label joinlobbynamelb;
+    @FXML Label LBLLobbyWelcome;
+    @FXML ComboBox CBjoinlobby;
+    @FXML TextField TFchat;
+    @FXML TextField TfCreatename;
+    @FXML TextField Tfvreatepassword;
+    @FXML Button Btcreatelobby;
+    @FXML Button btjoinlobby;
+    @FXML ListView Lblobby;
+    
+    
     //list voor de gemaakte lobby's te laten zien
     ArrayList<String> lobbyName;
 
@@ -62,28 +53,31 @@ public class SteampunkFXControllerlobby implements Initializable {
     private String ipAddress;
     private Registry registry = null;
     private IGameServer loginMock;
-
+ 
     private static final String bindingName = "serverMock";
 
-    public void setApp(SteampunkyFX application, Client client, String ipAddress, int portNumber) {
+    public void setApp(SteampunkyFX application,Client client,String ipAddress, int portNumber)
+    {
         this.clientInfo = client;
         this.main = application;
         LBLLobbyWelcome.setText("Welcome: " + client.getUser());
-
+        
         this.ipAddress = ipAddress;
         this.portNumber = portNumber;
         this.main = application;
-
+        
         clientInfo = new Client();
         // Print IP address and port number for registry
         System.out.println("Client: IP Address: " + this.ipAddress);
         System.out.println("Client: Port number " + this.portNumber);
-
+        
         Serverlogin();
-
+       
+        
     }
-
-    public void Serverlogin() {
+    
+    public void Serverlogin()
+    {
         // Locate registry at IP address and port number
         try {
             registry = LocateRegistry.getRegistry(this.ipAddress, this.portNumber);
@@ -92,7 +86,7 @@ public class SteampunkFXControllerlobby implements Initializable {
             System.out.println("Client: RemoteException: " + ex.getMessage());
             registry = null;
         }
-
+        
         // Print result locating registry
         if (registry != null) {
             System.out.println("Client: Registry located");
@@ -114,7 +108,7 @@ public class SteampunkFXControllerlobby implements Initializable {
                 loginMock = null;
             }
         }
-
+        
         if (loginMock != null) {
             System.out.println("Client: Login is bound");
         } else {
@@ -123,30 +117,34 @@ public class SteampunkFXControllerlobby implements Initializable {
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources) 
+    {   
         Clear();
         lobbyName = new ArrayList();
     }
 
+    
     //Maakt een lobby 
     @FXML
     public void AddLobby() {
         if (TfCreatename.getText().equals("")) {
-            JOptionPane.showMessageDialog(null, "Please enter a valid name.");
-        } else {
+            JOptionPane.showMessageDialog(null,"Please enter a valid name.");
+        }
+        else {
             try {
-                loginMock.createLobby(TfCreatename.getText(), Tfvreatepassword.getText(), this.clientInfo.getUser());
-
-                JOptionPane.showMessageDialog(null, "Lobby has been created");
-
-                main.gotoGameRoomselect(this.clientInfo, this.ipAddress, this.portNumber);
-            } catch (Exception ex) {
-                JOptionPane.showMessageDialog(null, "Lobby creation has failed" + ex.getMessage());
-                System.out.println("Failed" + ex.getMessage());
+                loginMock.createLobby(TfCreatename.getText(), Tfvreatepassword.getText(),this.clientInfo.getUser());
+                
+                JOptionPane.showMessageDialog(null,"Lobby has been created");
+                
+                main.gotoGameRoomselect(this.clientInfo,this.ipAddress,this.portNumber);            
+            }
+            catch (Exception ex) {
+                JOptionPane.showMessageDialog(null,"Lobby creation has failed" + ex.getMessage());
+                        System.out.println("Failed" + ex.getMessage());
             }
         }
     }
-
+    
     @FXML
     public void AddChatmessage() {
 //        if (TFchat.getText().equals("")) {
@@ -179,6 +177,7 @@ public class SteampunkFXControllerlobby implements Initializable {
 //        Lblobby.setItems(FXCollections.observableList(lobbyName));
 //        CBjoinlobby.setItems(FXCollections.observableList(lobbyName));
 //    }
+    
     public void Clear() {
         Lblobby.getItems().clear();
         CBjoinlobby.getItems().clear();
